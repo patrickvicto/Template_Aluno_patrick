5fecc8642ca0aaf415504e6dd15ab6c8bf8a429f
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-05-11 15:03:04 -0300

    semana 4 curriculo, com js

---
+++ 03_AUT_EST_ENTREGA/semana 4/curriculo/curriculo.js
@@ -0,0 +1,4 @@
+function revelar(){
+document.getElementById("info").innerHTML = "High School Canada (formação bilíngue) com aulas de:";
+document.getElementById("list").innerHTML =`<li>Negócios</li><li>Ciências Ambientais</li><li>Escrita</li><li>Liderança</li><li>Questões mundiais</li>`;
+}
