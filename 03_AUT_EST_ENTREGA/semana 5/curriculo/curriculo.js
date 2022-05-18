e45aba73bce93677365e890767dd5b20b18938bd
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-05-18 15:47:07 -0300

    curriculo semana 5 e tutorial semana 5

---
+++ 03_AUT_EST_ENTREGA/semana 5/curriculo/curriculo.js
@@ -0,0 +1,4 @@
+function revelar(){
+$('#info').html("High School Canada (formação bilíngue) com aulas de:");
+$('#list').html(`<li>Negócios</li><li>Ciências Ambientais</li><li>Escrita</li><li>Liderança</li><li>Questões mundiais</li>`);
+}
