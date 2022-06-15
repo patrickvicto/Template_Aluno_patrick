7aa36f11895ec6f51ac9dc0cb756863c3dc7b395
patrickvicto <patrick.miranda@sou.inteli.edu.br>
2022-06-15 14:06:08 -0300

    exercicio 4 e 5

--- 04_AUT_EST_EX_OBRIGATORIOS/semana 9/frontend/ex9.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/semana 9/frontend/ex9.js
@@ -14,6 +14,7 @@ function centena() {
 }
 
 //Exercício 2
+//função para somar os dígitos de um numero
 function somar(){
     var n_numeros = document.getElementById('positivenumber').value
     var soma = 0
@@ -22,13 +23,48 @@ function somar(){
     }
     alert("A soma dos numeros é: " + soma)
 }
-//função para somar os dígitos de um numero
-
 
 //Exercício 3
-
+//função para ordenar três nomes em ordem alfabética
 function ordenar(){
     var ordenar = [document.getElementById('name1').value,document.getElementById('name2').value,document.getElementById('name3').value] 
     ordenar.sort()
-    alert("Nomes em ordem: " + ordenar)
+    alert("Nomes em ordem:" + ordenar)
+}
+
+//Exercício 4
+//função para exibir e calcular sequencia de fibonacci
+function fibo(){
+    var n = document.getElementById('n_numbers').value
+    var sequencia = [1,1]
+    if(n>0){
+        for(let i=2; i<n; i++){
+            sequencia[i] = sequencia[i-1] + sequencia[i-2]
+        }
+        alert("Sequencia de Fibonacci:" + sequencia)
+    }
+    else{
+        alert("O numero deve ser positivo")
+    }
+}
+
+//exercício 5
+//função para mostrar todos os números primos de um intervalo
+function primos(){
+    var inicio = document.getElementById('intervalo1').value
+    var fim = document.getElementById('intervalo2').value
+    var primos = []
+    
+    for(let i=inicio; i<=fim; i++){
+        var contador = 0
+        for(let a=1; a<=i; a++){
+            if (i%a == 0){
+                contador +=1
+            }
+        }
+        if (contador == 2){
+            primos.push(i)
+            }
+    }
+    alert("Números primos:" + primos)
 }
